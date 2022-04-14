4eea96e485c4f95eb7c2015e1dab0632aed31983
Tyler <tpaul9908@gmail.com>
2022-04-13 21:39:23 -0500

    Fixed bugs, added win condition and mine flagging

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.hidden ? "" : props.value}
+    <button className="square" onClick={props.onClick} onContextMenu={(e) => {
+      {props.onRightClick()}
+      e.preventDefault()
+    }}>
+      {props.flagged ? "f" : (props.hidden ? "" : props.value)}
     </button>
   );
 }
@@ -16,135 +19,31 @@ class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         hidden={this.props.hidden[i]}
+        flagged={this.props.flagged[i]}
         onClick={() => this.props.onClick(i)}
+        onRightClick={() => this.props.onRightClick(i)}
       />
     );
   }
 
   render() {
+    let rows =[]
+    for(let i=0;i<10;i++){
+      rows.push([])
+
+      for(let j=0;j<10;j++){
+        rows[i].push(this.renderSquare(10*i+j))
+      }
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
+        {rows.map((ele) =>{
+          return(
+          <div className="board-row">
+            {ele}
+        </div>)
+        })}
         </div>
-        <div className="board-row">
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(20)}
-          {this.renderSquare(21)}
-          {this.renderSquare(22)}
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
-          {this.renderSquare(25)}
-          {this.renderSquare(26)}
-          {this.renderSquare(27)}
-          {this.renderSquare(28)}
-          {this.renderSquare(29)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(30)}
-          {this.renderSquare(31)}
-          {this.renderSquare(32)}
-          {this.renderSquare(33)}
-          {this.renderSquare(34)}
-          {this.renderSquare(35)}
-          {this.renderSquare(36)}
-          {this.renderSquare(37)}
-          {this.renderSquare(38)}
-          {this.renderSquare(39)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(40)}
-          {this.renderSquare(41)}
-          {this.renderSquare(42)}
-          {this.renderSquare(43)}
-          {this.renderSquare(44)}
-          {this.renderSquare(45)}
-          {this.renderSquare(46)}
-          {this.renderSquare(47)}
-          {this.renderSquare(48)}
-          {this.renderSquare(49)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(50)}
-          {this.renderSquare(51)}
-          {this.renderSquare(52)}
-          {this.renderSquare(53)}
-          {this.renderSquare(54)}
-          {this.renderSquare(55)}
-          {this.renderSquare(56)}
-          {this.renderSquare(57)}
-          {this.renderSquare(58)}
-          {this.renderSquare(59)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(60)}
-          {this.renderSquare(61)}
-          {this.renderSquare(62)}
-          {this.renderSquare(63)}
-          {this.renderSquare(64)}
-          {this.renderSquare(65)}
-          {this.renderSquare(66)}
-          {this.renderSquare(67)}
-          {this.renderSquare(68)}
-          {this.renderSquare(69)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(70)}
-          {this.renderSquare(71)}
-          {this.renderSquare(72)}
-          {this.renderSquare(73)}
-          {this.renderSquare(74)}
-          {this.renderSquare(75)}
-          {this.renderSquare(76)}
-          {this.renderSquare(77)}
-          {this.renderSquare(78)}
-          {this.renderSquare(79)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(80)}
-          {this.renderSquare(81)}
-          {this.renderSquare(82)}
-          {this.renderSquare(83)}
-          {this.renderSquare(84)}
-          {this.renderSquare(85)}
-          {this.renderSquare(86)}
-          {this.renderSquare(87)}
-          {this.renderSquare(88)}
-          {this.renderSquare(89)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(90)}
-          {this.renderSquare(91)}
-          {this.renderSquare(92)}
-          {this.renderSquare(93)}
-          {this.renderSquare(94)}
-          {this.renderSquare(95)}
-          {this.renderSquare(96)}
-          {this.renderSquare(97)}
-          {this.renderSquare(98)}
-          {this.renderSquare(99)}
-        </div>
-      </div>
     );
   }
 }
@@ -155,6 +54,7 @@ class Game extends React.Component {
     const myArr = generateNewGame()
     const mySquares=myArr[0]
     const hiddenArray=myArr[1]
+    const flagged=Array(100).fill(null)
     this.state = {
       history: [
         {
@@ -163,10 +63,16 @@ class Game extends React.Component {
       ],
       stepNumber: 0,
       hidden: hiddenArray,
+      numMines: myArr[2],
+      flagged: flagged,
+      flagWon: false,
     };
   }
 
   handleClick(i) {
+    if(this.state.flagWon) return;
+    const flagged = this.state.flagged
+    if(flagged[i]) return;
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     let squares = current.squares.slice();
@@ -188,40 +94,47 @@ class Game extends React.Component {
       while (zeroStack.length > 0) {
         console.log(zeroStack)
         i = zeroStack.pop()
-        if (i - 11 > 0 && squares[i - 11] === 0 && hidden[i - 11] && ((i-11)/10===i/10-1)) {
+        if (i - 11 > 0 && squares[i - 11] === 0 && hidden[i - 11] && (i%10!==0)) {
           zeroStack.push(i - 11);
         }
         if (i - 10 > 0 && squares[i - 10] === 0 && hidden[i - 10]) {
           zeroStack.push(i - 10);
         }
-        if (i - 9 > 0 && squares[i - 9] === 0 && hidden[i - 9] && ((i-9)/10===i/10-1)) {
+        if (i - 9 > 0 && squares[i - 9] === 0 && hidden[i - 9] && (i%10!==9)) {
           zeroStack.push(i - 9);
         }
-        if (i - 1 > 0 && squares[i - 1] === 0 && hidden[i - 1] && ((i-1)/10===i/10)) {
+        if (i - 1 > 0 && squares[i - 1] === 0 && hidden[i - 1] && (i%10!==0)) {
           zeroStack.push(i - 1);
         }
-        if (i + 1 < 100 && squares[i + 1] === 0 && hidden[i + 1] && ((i+1)/10===i/10)) {
+        if (i + 1 < 100 && squares[i + 1] === 0 && hidden[i + 1] && (i%10!==9)) {
           zeroStack.push(i + 1);
         }
-        if (i + 9 < 100 && squares[i + 9] === 0 && hidden[i + 9] &&((i+9)/10===i/10+1)) {
+        if (i + 9 < 100 && squares[i + 9] === 0 && hidden[i + 9] &&(i%10!==0)) {
           zeroStack.push(i + 9);
         }
         if (i + 10 < 100 && squares[i + 10] === 0 && hidden[i + 10]) {
           zeroStack.push(i + 10);
         }
-        if (i + 11 < 100 && squares[i + 11] === 0 && hidden[i + 11]&&((i+11)/10===i/10+1)) {
+        if (i + 11 < 100 && squares[i + 11] === 0 && hidden[i + 11]&&((i+11)%10!==0)) {
           zeroStack.push(i + 11);
         }
-        hidden[i - 11] = hidden[i-11] ? (((i-11)/10 === i/10-1) ? 0 : 1) : 0;
+        hidden[i - 11] = hidden[i-11] ? ((i%10!==0) ? 0 : 1) : 0;
         hidden[i - 10] = 0;
-        hidden[i - 9] = hidden[i-9] ? (((i-9)/10 === i/10-1) ? 0 : 1) : 0;
-        hidden[i - 1] = hidden[i-1] ? (((i-1)/10=== i/10) ? 0 : 1) : 0;
-        hidden[i + 1] = hidden[i+1] ? (((i+1)/10=== i/10) ? 0 : 1) : 0;
-        hidden[i + 9] = hidden[i+9] ? (((i+9)/10=== i/10+1) ? 0 : 1) : 0;
+        hidden[i - 9] = hidden[i-9] ? ((i%10!==9) ? 0 : 1) : 0;
+        hidden[i - 1] = hidden[i-1] ? ((i%10!==0) ? 0 : 1) : 0;
+        hidden[i + 1] = hidden[i+1] ? ((i%10!==9) ? 0 : 1) : 0;
+        hidden[i + 9] = hidden[i+9] ? ((i%10!==0) ? 0 : 1) : 0;
         hidden[i + 10] = 0;
-        hidden[i + 11] = hidden[i+11] ? (((i+11)/10=== i/10+1) ? 0 : 1) : 0;
+        hidden[i + 11] = hidden[i+11] ? (((i+11)%10!==0) ? 0 : 1) : 0;
       }
 
+    }
+    let flagWon=true
+    for(let i=0;i<100;i++){
+      if(hidden[i] && squares[i]!=="X"){
+        flagWon=false
+        break
+      }
     }
     this.setState({
       history: history.concat([
@@ -230,8 +143,18 @@ class Game extends React.Component {
         }
       ]),
       stepNumber: history.length,
-      hidden: hidden
+      hidden: hidden,
+      flagWon: flagWon,
     });
+  }
+
+  handleRightClick(i){
+    const flagged=this.state.flagged.slice()
+    const hidden=this.state.hidden.slice()
+    flagged[i]= !hidden[i] ? 0 : (flagged[i] ? 0 : 1)
+    this.setState({
+      flagged: flagged,
+    })
   }
 
   jumpTo(step) {
@@ -246,22 +169,11 @@ class Game extends React.Component {
     const hidden = this.state.hidden;
     const winner = calculateGameState(current.squares, hidden);
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
     let status;
     if (winner) {
       status = "Winner: " + !winner;
     } else {
-      status = "";
+      status = this.state.flagWon ? "You win" : "Number of Mines: ".concat(this.state.numMines.toString());
     }
 
     return (
@@ -271,11 +183,12 @@ class Game extends React.Component {
             squares={current.squares}
             onClick={i => this.handleClick(i)}
             hidden={this.state.hidden}
+            flagged={this.state.flagged}
+            onRightClick={i => this.handleRightClick(i)}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -286,7 +199,7 @@ class Game extends React.Component {
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Game tab="home" />);
+root.render(<Game tab="home"/>);
 
 function calculateGameState(squares, hidden) {
   for (let i = 0; i < 100; i++) {
@@ -300,28 +213,31 @@ function calculateGameState(squares, hidden) {
 function generateNewGame() {
   let mySquares = Array(100).fill(null);
   let hiddenArray = Array(100).fill(null);
-  for (let i = 0; i < 100; i++) {
-    if (Math.random() < .2) {
-      mySquares[i] = "X"
+  let numMines=15;
+  let minesToPlace=numMines;
+  while(minesToPlace>0){
+    let pos = Math.floor(Math.random()*100)
+    if(mySquares[pos]!=="X"){
+      mySquares[pos]="X"
+      minesToPlace--
     }
   }
   for (let i = 0; i < 100; i++) {
-    console.log(i/10)
     if (!mySquares[i]) {
       let sum = 0
-      if (i - 11 > 0 && mySquares[i - 11] == "X" && (Math.floor((i-11)/10)===Math.floor(i/10)-1)) sum += 1;
+      if (i - 11 > 0 && mySquares[i - 11] == "X" && (i%10!==0)) sum += 1;
       if (i - 10 > 0 && mySquares[i - 10] == "X") sum += 1;
-      if (i - 9 > 0 && mySquares[i - 9] == "X" && ((i-9)/10===i/10-1)) sum += 1;
-      if (i - 1 > 0 && mySquares[i - 1] == "X" && ((i-1)/10===i/10))sum += 1;
-      if (i + 1 < 100 && mySquares[i + 1] == "X" && ((i+1)/10===i/10)) sum += 1;
-      if (i + 9 < 100 && mySquares[i + 9] == "X" && ((i+9)/10===i/10+1)) sum += 1;
+      if (i - 9 > 0 && mySquares[i - 9] == "X" && (i%10!==9)) sum += 1;
+      if (i - 1 > 0 && mySquares[i - 1] == "X" && (i%10!==0)) sum += 1;
+      if (i + 1 < 100 && mySquares[i + 1] == "X" && (i%10!==9)) sum += 1;
+      if (i + 9 < 100 && mySquares[i + 9] == "X" && (i%10!==0)) sum += 1;
       if (i + 10 < 100 && mySquares[i + 10] == "X") sum += 1;
-      if (i + 11 < 100 && mySquares[i + 11] == "X" && ((i+11)/10===i/10+1)) sum += 1;
+      if (i + 11 < 100 && mySquares[i + 11] == "X" && ((i+11)%10!==0)) sum += 1;
       mySquares[i] = sum
     }
   }
   for (let i = 0; i < 100; i++) {
     hiddenArray[i] = 1
   }
-  return [mySquares,hiddenArray];
+  return [mySquares,hiddenArray,numMines];
 }
